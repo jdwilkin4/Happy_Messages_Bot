b526b9468af25ee81949dfb61edee0963cdd31a3
jdwilkin4 <jwilkin4@hotmail.com>
2021-08-20 20:28:51 -0700

    testing node cron to send message every minute

--- mom-messages/afternoon-messages.js
+++ mom-messages/afternoon-messages.js
@@ -1,19 +1,19 @@
-const momEmail = process.env.MOM_EMAIL;
+/* const momEmail = process.env.MOM_EMAIL;
 const jessEmail = process.env.ADMIN_EMAIL;
 const axios = require('axios').default;
 
 
 //needs work
-/* const randomQuote = axios.get('https://free-quotes-api.herokuapp.com/')
+ const randomQuote = axios.get('https://free-quotes-api.herokuapp.com/')
     .then(response => console.log(response.data))
-    .catch(error => console.error(error)); */
+    .catch(error => console.error(error));
 
 
-/* const goodAfternoonMsg = {
+ const goodAfternoonMsg = {
     from: jessEmail,
     to: jessEmail, //temporarily testing with my email
     cc: jessEmail,
     subject: 'Good Afternoon!',
     text: `Did you drink your water today? This was sent from my bot :) ${randomQuote}`
-} */
-module.exports = goodAfternoonMsg;
+}
+module.exports = goodAfternoonMsg; */
