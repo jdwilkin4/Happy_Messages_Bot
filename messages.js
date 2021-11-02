46e9301527f499bb703405acf1f40dc470fcdc61
jdwilkin4 <jwilkin4@hotmail.com>
2021-11-01 23:44:32 -0700

    creating Email Message object constructor for the multiple object email instances

---
+++ messages.js
@@ -0,0 +1,30 @@
+// html messages
+const morningMessage = `
+<h1>Good Morning!</h1>
+<p>Hope you have a beautiful day</p>
+`;
+
+const drinkWater = `<h1>Did you drink your water today?</h1>`;
+
+const afternoonMessage = `
+<h1>Good Afternoon!</h1>
+<p>Just wanted to say I love you. Hope your day is going well.</p>
+`;
+
+const eveningMessage = `
+<h1>Good Evening!</h1>
+<p>Hope your day went well. Make sure to get plenty of rest. And remember you are awesome.</p>
+`;
+
+const birthdayMessage = `
+<h1>Happy Birthday</h1>
+`;
+
+module.exports = { morningMessage, drinkWater, afternoonMessage, birthdayMessage, eveningMessage }
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const nodeMailer = require('nodemailer');
+const nodeCron = require('node-cron');
+const momEmail = process.env.MOM_EMAIL;
+const jessEmail = process.env.ADMIN_EMAIL;
+const jessPassword = process.env.ADMIN_PASSWORD;
+const { morningMessage, drinkWater, afternoonMessage, birthdayMessage, eveningMessage } = require('./messages');
+
+//object constructor for emails
+function EmailMessage(from, to, cc, subject, html) {
+  this.from = from;
+  this.to = to;
+  this.cc = cc;
+  this.subject = subject;
+  this.html = html
+};
+
+//object instances for sending the different emails
+let sendMomBirthdayMsg = new EmailMessage(jessEmail, momEmail, jessEmail, 'Happy Birthday', birthdayMessage);
+let sendMorningMsg = new EmailMessage(jessEmail, momEmail, jessEmail, 'Good Morning!', morningMessage);
+let sendAfternoonMsg = new EmailMessage(jessEmail, momEmail, jessEmail, "Good Afternoon", afternoonMessage);
+let sendEveningMsg = new EmailMessage(jessEmail, momEmail, jessEmail, 'Good Evening', eveningMessage);
+let sendDrinkWater = new EmailMessage(jessEmail, momEmail, jessEmail, "Please Drink water", drinkWater);
+
+//transporter to send emails
+let transporter = nodeMailer.createTransport({
+  service: 'outlook',
+  port: 587,
+  secure: false,
+  auth: {
+    user: jessEmail,
+    pass: jessPassword
+  }
+});
+
+//verify email connection for admin email
+transporter.verify((error) => {
+  error ? console.log(`There was an error message: ${error}`) : console.log('Ready to send email')
+});
+
+//setup the cron schedule to delivery mail every day at 9am, noon and 6pm PST
+nodeCron.schedule('0 9 * * *', () => {
+  async function morningMsg() {
+    let info = await transporter.sendMail(sendMorningMsg)
+    console.log(`Message send: ${info.messageId}`)
+  }
+  morningMsg().catch(console.error);
+});
+
+nodeCron.schedule('0 12 * * *', () => {
+  async function afternoonMsg() {
+    let info = await transporter.sendMail(sendAfternoonMsg)
+    console.log(`Message send: ${info.messageId}`)
+  }
+  afternoonMsg().catch(console.error);
+});
+
+nodeCron.schedule('0 18 * * *', () => {
+  async function eveningMsg() {
+    let info = await transporter.sendMail(sendEveningMsg)
+    console.log(`Message send: ${info.messageId}`)
+  }
+  eveningMsg().catch(console.error);
+});
+
+
+//setup the cron schedule to delivery email on mom's birthday
+nodeCron.schedule('0 8 20 May *', () => {
+  async function momBirthday() {
+    let info = await transporter.sendMail(sendMomBirthdayMsg);
+    console.log(`Message sent:${info.messageId}`);
+  }
+  momBirthday().catch(console.error);
+});
+
+//setup the cron schedule to delivery email remainder to drink water every 4 hours
+nodeCron.schedule('* */4 * * *', () => {
+  async function drinkWaterMsg() {
+    let info = await transporter.sendMail(sendDrinkWater)
+    console.log(`Message send: ${info.messageId}`)
+  }
+  drinkWaterMsg().catch(console.error);
+});
+
+
+
+
